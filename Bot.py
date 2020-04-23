77fe4d42f8233ab56d74ddd5f5a5f25d30e7496c
Jeevs09 <jeevan.bhoot@yahoo.com>
2020-04-23 14:35:51 +0100

    Formatting of output.

--- Bot.py
+++ Bot.py
@@ -18,7 +18,7 @@ async def play(ctx, *args):
     seperator = ' '
     search_query = seperator.join(args)
     results = YT(search_query, max_results = 5).export()
-    output = '''1. {name1} \n 2. {name2} \n 3. {name3} \n 4. {name4} \n 5. {name5}\
+    output = '''1. {name1}\n2. {name2}\n3. {name3}\n4. {name4}\n5. {name5}\
     '''.format(name1=results[0][0], name2=results[1][0], name3=results[2][0], name4=results[3][0], name5=results[4][0])
     await ctx.send(output)
 
